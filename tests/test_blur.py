d27399b0f31ec339b7b157c5adf07c00ab1a0c68
Jeon <214285@jnu.ac.kr>
2026-04-24 19:12:29 +0900

    feature: add test file

---
+++ tests/test_blur.py
@@ -0,0 +1,63 @@
+import time
+from playwright.sync_api import Page
+import subprocess, pytest
+
+test_image_path = "tests/test.png"
+
+@pytest.fixture(scope="session", autouse=True)
+def start_test_server():
+    server_process = subprocess.Popen(["uvicorn", "app.main:app", "--port", "8000"])
+    time.sleep(2)
+    yield
+    server_process.terminate()
+
+def get_canvas_data(page: Page):
+        return page.evaluate('document.getElementById("Canvas").toDataURL()')
+
+def test_blur(page: Page):
+    # 이미지 불러오기
+    page.goto("http://localhost:8000")
+    page.set_input_files("#imageInput", test_image_path)
+    page.wait_for_timeout(500)
+
+    canvas = page.locator("#Canvas")
+    box = canvas.bounding_box()
+    assert box is not None
+
+    initial_info = page.evaluate("""() => {
+        const c = document.getElementById('Canvas');
+        return { width: c.width, height: c.height };
+    }""")
+    initial_w = initial_info['width']
+    initial_h = initial_info['height']
+                                 
+    pure_image_data = get_canvas_data(page)
+
+    # 임의의 박스 드래그
+    page.mouse.move(box['x'] + 50, box['y'] + 60)
+    page.mouse.down()
+    page.mouse.move(box['x'] + 650, box['y'] + 340)
+    page.mouse.up()
+
+    boxed_image_data = get_canvas_data(page)
+
+    # 흐리게 버튼 클릭
+    page.get_by_role("button", name="흐리게").click()
+
+    page.wait_for_timeout(100)
+
+    # 캔버스 정보 불러오기
+    final_info = page.evaluate("""() => {
+        const c = document.getElementById('Canvas');
+        return {
+            width: c.width, height: c.height
+        };
+    }""")
+
+    after_image_data = get_canvas_data(page)
+
+    assert final_info['width'] == initial_w
+    assert final_info['height'] == initial_h
+
+    assert after_image_data != pure_image_data  # 원본이랑 동일한지
+    assert after_image_data != boxed_image_data
